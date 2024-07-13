371c7b38de6fba7a87ccfb38441254ed33ff29b3
birdhimself <git@wuul.eu>
2024-07-13 11:30:00 +0200

    Install and use Vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const drawer = ref(false);
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar density="compact">
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-app-bar-title>Valheim Armory</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer">
+      <v-list nav>
+        <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox" />
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
