7e9e8273d2759c9f9794b432baa1b020c0b47140
birdhimself <git@wuul.eu>
2024-11-08 00:10:04 +0100

    Add page title and favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: "Valheim Armory",
+  link: [
+    {
+      rel: "icon",
+      type: "image/svg+xml",
+      href: "/favicon.svg",
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
