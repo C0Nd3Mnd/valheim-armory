4674ec88943faf44fe15f1b56cabe29c15a23335
birdhimself <git@wuul.eu>
2024-11-10 13:33:04 +0100

    Add damage steps presets

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,67 @@ import { useCharacterStore } from "~/stores/character";
 
 const characterStore = useCharacterStore();
 const damageStore = useDamageStore();
+
+const damageStepsPresets = [
+  [
+    {
+      label: "Meadows",
+      click: () => {
+        damageStore.start = 5;
+        damageStore.end = 10;
+        damageStore.stepSize = 1;
+      },
+    },
+    {
+      label: "Black Forest",
+      click: () => {
+        damageStore.start = 10;
+        damageStore.end = 70;
+        damageStore.stepSize = 10;
+      },
+    },
+    {
+      label: "Swamp",
+      click: () => {
+        damageStore.start = 20;
+        damageStore.end = 120;
+        damageStore.stepSize = 10;
+      },
+    },
+    {
+      label: "Mountains",
+      click: () => {
+        damageStore.start = 40;
+        damageStore.end = 110;
+        damageStore.stepSize = 10;
+      },
+    },
+    {
+      label: "Plains",
+      click: () => {
+        damageStore.start = 85;
+        damageStore.end = 130;
+        damageStore.stepSize = 5;
+      },
+    },
+    {
+      label: "Mistlands",
+      click: () => {
+        damageStore.start = 50;
+        damageStore.end = 200;
+        damageStore.stepSize = 10;
+      },
+    },
+    {
+      label: "Ashlands",
+      click: () => {
+        damageStore.start = 120;
+        damageStore.end = 240;
+        damageStore.stepSize = 15;
+      },
+    },
+  ],
+];
 </script>
 
 <template>
@@ -12,6 +73,14 @@ const damageStore = useDamageStore();
       <UInput v-model="characterStore.health" type="number" />
     </UFormGroup>
     <h2 class="text-xl">Damage steps</h2>
+    <UFormGroup>
+      <UDropdown :items="damageStepsPresets">
+        <UButton
+          label="Load Preset"
+          trailing-icon="i-pepicons-print-angle-down"
+        />
+      </UDropdown>
+    </UFormGroup>
     <div class="grid grid-cols-2 md:grid-cols-3 gap-2">
       <UFormGroup label="Start">
         <UInput v-model="damageStore.start" type="number" />
